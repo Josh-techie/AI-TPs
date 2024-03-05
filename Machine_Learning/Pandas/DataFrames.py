1a106c1351ad2d1f138560dd6a6f94a04a3327ab
Josh-techie <youse2002@outlook.fr>
2024-03-05 09:37:35 +0100

    🐼 Panda-Intro-Code

--- Machine_Learning/Pandas/DataFrames.py
+++ Machine_Learning/Pandas/DataFrames.py
@@ -2,30 +2,32 @@
 
 import pandas as pd
 
+# create dictionary with 2 keys and 4 values for each key
 data = {"apples": [3, 2, 0, 1], "oranges": [0, 3, 7, 2]}
 
-# we will be visualiging the data in a table format
-
+# ("apples" and "oranges") will become the column names in the DataFrame
 purchases = pd.DataFrame(data)
-# print(repr(purchases))
+# printing the data
+# print(purchases)
 
-########################## CREATING OUR DATAFRAME ##########################
+#     # OUPUT
+# ########################## CREATING OUR DATAFRAME ##########################
 
-    # USE CASE `1`: Creating a DataFrame from a dictionary
-# List of lists
-data1 = [["tom", 25], ["krish", 30], ["nick", 26], ["juli", 22]]
+#     # USE CASE `1`: Creating a DataFrame from a dictionary
+# # List of lists
+# data1 = [["tom", 25], ["krish", 30], ["nick", 26], ["juli", 22]]
 
-# Create the pandas DataFrame
-df = pd.DataFrame(data1, columns=["Name", "Age"])
-# print(df)
+# # Create the pandas DataFrame
+# df = pd.DataFrame(data1, columns=["Name", "Age"])
+# # print(df)
 
-    # USE CASE `2`: Creating a DataFrame with index Parameter ##########################
-purchases = pd.DataFrame(data, index=["June", "Robert", "Lily", "David"])
-print(purchases)
+#     # USE CASE `2`: Creating a DataFrame with index Parameter ##########################
+# purchases = pd.DataFrame(data, index=["June", "Robert", "Lily", "David"])
+# print(purchases)
 
-    # OUPUT
-#         apples  oranges
-# June         3        0
-# Robert       2        3
-# Lily         0        7
-# David        1        2
+#     # OUPUT
+# #         apples  oranges
+# # June         3        0
+# # Robert       2        3
+# # Lily         0        7
+# # David        1        2
