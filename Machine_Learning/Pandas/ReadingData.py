980570fad03ab352c294b729518b2c86324843ff
Josh-techie <youse2002@outlook.fr>
2024-03-05 10:02:41 +0100

    🤍 Head-CSV-Reading-Data

--- Machine_Learning/Pandas/ReadingData.py
+++ Machine_Learning/Pandas/ReadingData.py
@@ -3,16 +3,23 @@
 import pandas as pd
 
 # read the data
-data = pd.read_csv("data.csv")  # yeah as simple as that 😀
+# data = pd.read_csv("data.csv")  # yeah as simple as that 😀
 
-# but wait how do I know if the data was read correctly?
-# let's print the first 5 rows of the data
-print(data.head())
+# # but wait how do I know if the data was read correctly?
+# # let's print the first 5 rows of the data
+# print(data.head())
 
 # if you run the code you can see it's all good.
 
-    # hold up you have to suffer bro, before you learn! 😂
-    # if you take a look at the data you'll see that the csv file is well structured and formatted
-    # but what if the file is not well formatted, for examples instead of using `,` we used `;`
+# hold up you have to suffer bro, before you learn! 😂
+# if you take a look at the data you'll see that the csv file is well structured and formatted
+# but what if the file is not well formatted, for examples instead of using `,` we used `;`
 
-# Well I got you covered, you can use the `sep` parameter to specify the delimiter    
+# Well I got you covered, you can use the `sep` parameter to specify the delimiter
+
+data1 = pd.read_csv("data1.csv", sep=";")
+# here to print and this time we'll print the first 10 rows\
+# just getting acquainted with head() method
+print(data1.head(5))
+# or print the whole thing
+print(data1)
